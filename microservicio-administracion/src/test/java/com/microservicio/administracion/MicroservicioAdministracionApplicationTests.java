3c5de62045728c4ebc76e6cf1d8abb2e0875857d
AndresMoranFlorentin <moranandres729@gmail.com>
2024-06-05 12:18:07 -0300

    creacion del esqueleto basico del microservicio

---
+++ microservicio-administracion/src/test/java/com/microservicio/administracion/MicroservicioAdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.administracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAdministracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
